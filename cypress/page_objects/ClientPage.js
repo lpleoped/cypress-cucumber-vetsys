269249ffe0a4e2f684d442683748fbb4c1d6af80
Leonardo Pedreira <lp.leonardo.pedreira@hotmail.com>
2024-09-11 17:45:08 -0300

    Implement login flow, generate valid ID, and navigate to Clients page
    - Completed the login functionality with valid user credentials.
    - Added utility to generate a valid Uruguayan ID (CI).
    - Implemented full flow from login to client management navigation.

--- cypress/page_objects/ClientPage.js
+++ cypress/page_objects/ClientPage.js
@@ -1,7 +1,15 @@
 const { first } = require("lodash");
 
 class ClientPage {
-   
+    elements = {
+        clientsMenuOption: () => cy.contains('li', 'Clientes'),
+        title: () =>  cy.get('h1')
+    }
+
+    navigateToClientsPage(){
+        this.elements.clientsMenuOption().click();
+        this.elements.title().should('contain', 'Clientes');
+    }    
 }
 
 module.exports = new ClientPage();
